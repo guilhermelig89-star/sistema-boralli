ff4d1836f9d5193908be642f043f94a6f1bb9a6a
guilhermelig89-star <guilhermelig89@gmail.com>
2026-05-08 11:41:25 -0300

    Add shared Firebase config

---
+++ src/shared/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDakVTw38aVT5Szi0NCg3FTHyD6y0heXu8",
+  authDomain: "sistema-boralli-v1.firebaseapp.com",
+  projectId: "sistema-boralli-v1",
+  storageBucket: "sistema-boralli-v1.firebasestorage.app",
+  messagingSenderId: "27408903456",
+  appId: "1:27408903456:web:f7a977ac185d35976b7b45",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
